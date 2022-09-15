063e5f6072b0470427e957939baa029f40b86860
Thomas Segers <44966255+Thomassegers14@users.noreply.github.com>
2022-09-15 18:07:02 +0300

    adjust php

--- form-to-email.php
+++ form-to-email.php
@@ -1,24 +1,35 @@
 <?php
-
-// accessing form submission data
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
-
-// composing email messaage
-$email_from = 'segers.thomas@telenet.be';
-
-$email_subject = "New Form submission";
-
-$email_body = "You have received a new message from the user $name.\n".
-                          "Here is the message:\n $message".
-
-// sending the email
-$to = "segers.thomas@telenet.be";
-
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
+$errors = [];
+$errorMessage = '';
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
+    }
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
+    }
+    if (empty($errors)) {
+        $toEmail = 'example@example.com';
+        $emailSubject = 'New email from your contant form';
+        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
+        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+        $body = join(PHP_EOL, $bodyParagraphs);
+        if (mail($toEmail, $emailSubject, $body, $headers)) {
+            header('Location: thank-you.html');
+        } else {
+            $errorMessage = 'Oops, something went wrong. Please try again later';
+        }
+    } else {
+        $allErrors = join('<br/>', $errors);
+        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    }
+}
 ?>
